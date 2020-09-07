40be5add5f88255dc05acee6ceae88a754a67d77
IvanTunyov <tunev.i@phystech.edu>
2020-09-07 18:02:49 +0300

    Initial commit

---
+++ kmb.py
@@ -0,0 +1,31 @@
+from socket import *
+import sys
+
+print("Enter host IP or type")
+host = input()
+
+print("Enter port")
+port = int(input())
+
+if "-s" in sys.argv:
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    serverSocket.bind(('', port))
+    serverSocket.listen(1)
+
+    while 1:
+        connectionSocket, addr = serverSocket.accept()
+        if addr[0] == host:
+            print("request from host:", addr)
+            connectionSocket.sendall((addr[0] + ' ' + addr[1]).encode())
+
+    connectionSocket.close()
+
+if "-c" in sys.argv:
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((host, port))
+
+    message = clientSocket.recv(2048)
+    print(message.decode())
+    clientSocket.close()
+
