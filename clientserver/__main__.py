09fef6187055dd5839cafe769b9ddf628ba294bb
IvanTunyov <tunev.i@phystech.edu>
2020-10-13 16:32:06 +0300

    cosmetic changes

--- clientserver/__main__.py
+++ clientserver/__main__.py
@@ -10,13 +10,12 @@ if __name__ == '__main__':
     PARSER.add_argument('ip', type=str)
     PARSER.add_argument('port', type=int)
 
-    PARSER.add_argument('-cn', dest='use_connect', action='store_true') #for using connect() method in UDP regime
+    #for using connect() method in UDP regime
+    PARSER.add_argument('-c', dest='use_connect', action='store_true')
 
-    PARSER.add_argument('-rs', dest='raw_socket', action='store_true')
+    PARSER.add_argument('-r', dest='raw_socket', action='store_true')
 
-    CONNECT_ROLE = PARSER.add_mutually_exclusive_group()
-    CONNECT_ROLE.add_argument('-s', dest='server', action='store_true')
-    CONNECT_ROLE.add_argument('-c', dest='client', action='store_true')
+    PARSER.add_argument('-s', dest='server', action='store_true')
 
     CONNECT_TYPE = PARSER.add_mutually_exclusive_group()
     CONNECT_TYPE.add_argument('-u', dest='udp_connect', action='store_true')
@@ -27,21 +26,19 @@ if __name__ == '__main__':
     HOST = ARGS.ip
     PORT = ARGS.port
 
-    if ARGS.client:
-        if ARGS.udp_connect:
-            if ARGS.raw_socket:
-                udp.raw_client(HOST, PORT)
-            elif ARGS.use_connect:
-                udp.connect_client(HOST, PORT)
-            else:
-                udp.client(HOST, PORT)
-        else:
-            tcp.client(HOST, PORT)
-    else:
-        if ARGS.udp_connect:
-            if ARGS.raw_socket:
-                udp.raw_server(HOST, PORT)
-            else:
-                udp.server(HOST, PORT)
+    if ARGS.server:
+        if ARGS.raw_socket:
+            udp.raw_server(HOST, PORT)
+        elif ARGS.udp_connect:
+            udp.server(HOST, PORT)
         else:
             tcp.server(HOST, PORT)
+    else:
+        if ARGS.raw_socket:
+            udp.raw_client(HOST, PORT)
+        elif ARGS.use_connect:
+            udp.connect_client(HOST, PORT)
+        elif ARGS.udp_connect:
+            udp.client(HOST, PORT)
+        else:
+            tcp.client(HOST, PORT)
